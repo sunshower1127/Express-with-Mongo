dc8e6689fa5a89fbaedcf0c074e8dac8c372dda7
Sunwoo Kim (MacBook) <sunshower1127@gmail.com>
2024-08-25 19:32:34 +0900

    express 세팅

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import mongoose from "mongoose";
+import express from "express";
+import path from "path";
 
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home", { message: "This is Home of Yelp Camp" });
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
